5ff4ea19d0aa4f0a388b21452fbd064061d745d7
Scource <tomekwa@gmail.com>
2021-06-19 14:24:47 +0200

    deleted frontend folder

--- fullsite/frontend/src/actions/axiosConfig.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const axiosConfig = axios.create({
-baseURL: 'http://localhost:8000',
-headers: {Authorization: `Token ${localStorage.getItem('token')}`}
-});
-
-export default axiosConfig;
